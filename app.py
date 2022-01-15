411be402c15e4cadd64fb426bce733e8e02622cf
Arno Richard <arno.richard@gmail.com>
2022-01-15 02:15:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World, from inside the original application!"
+    return "This is a small, small World!"
 
 
 if __name__ == "__main__":
